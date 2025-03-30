d6330f37337beabc48ed4f8936b3b53f084f9a88
renapramdya <renapramudya1884@gmail.com>
2025-03-30 16:02:11 +0700

    Perbaiki routing

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
     return redirect()->route('login');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-    Route::get('something', function () {
-        return Inertia::render('something');
-    })->name('something');
+    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::inertia('something', 'something')->name('something');
 });
 
 require __DIR__ . '/settings.php';
